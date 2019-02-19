e593414f4b6cdcdd32529bf592b3982e294755f7
prossini1 <46937333+prossini1@users.noreply.github.com>
2019-02-19 12:55:43 -0600

    Add files via upload

---
+++ getting_started.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# firt line of code
+print('Hello Word') # print(nathing)
+
+
+# In[3]:
+
+
+# this is a comment
+
+
+# In[4]:
+
+
+print('I am', 31,'years old')
+
+
+# In[5]:
+
+
+a = 1
+
+
+# In[6]:
+
+
+a
+
+
+# In[7]:
+
+
+b = 3
+
+
+# In[8]:
+
+
+print(a,b)
+
+
+# In[9]:
+
+
+print(a b)
+
+
+# In[10]:
+
+
+mu, sigma = 0, 0.2
+
+
+# In[11]:
+
+
+mu
+
+
+# In[12]:
+
+
+print('sigma is equal to',sigma)
+
+
+# In[23]:
+
+
+
+# define inputs
+cars = 60
+sits_per_car = 4
+passenger = 185
+
+# make calculations
+carpool_capacity = cars * sits_per_car
+sits_available = carpool_capacity - passenger
+
+# print results
+print("there are",cars,"cars and remaing only",sits_available,"sits_available")
+
+
+# In[26]:
+
+
+print("there are "+ str(cars) +" cars and remaing only "+ str(sits_available) +" sits_available")
+
+
+# In[27]:
+
+
+print(type(cars))
+
+
+# In[28]:
+
+
+print(type(5.4))
+
+
+# In[30]:
+
+
+print(type(1000))
+
+
+# In[34]:
+
+
+weigth = 82 # kg
+weigth_kg_to_pu = weigth*2.205
+
+print(weigth_kg_to_pu)
+print("My weigth is",round(weigth_kg_to_pu,0),"pounds")
+
+
+# In[ ]:
+
+
+
+
---
+++ testexe.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('hello word')
+
+
+# In[3]:
+
+
+print('Hoka Wally')
+
